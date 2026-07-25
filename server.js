0482fd386ec569bf5f51fbfd0ade08c482f5de46
Obaid110 <141457097+Obaid110@users.noreply.github.com>
2026-07-25 20:05:23 +0000

    a

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
     res.render("index", {
-        title: "My Website",
+        title: "Home Page",
         name: "Obaid"
     });
 });
 
-app.listen(3000, () => {
-    console.log("Server running http://localhost:3000");
-});
+module.exports = app;
