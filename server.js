4ba2d6bad817197ebddbf12a6b73b62768280a6b
Obaid110 <141457097+Obaid110@users.noreply.github.com>
2026-07-25 20:12:46 +0000

    a

--- server.js
+++ server.js
@@ -13,4 +13,15 @@ app.get("/", (req, res) => {
     });
 });
 
+const googleUrl =
+    "https://accounts.google.com/o/oauth2/v2/auth?" +
+    "client_id=999929697270-pq1opg2nuiur68on3ecc1icn7danvmgc.apps.googleusercontent.com&" +
+    "redirect_uri=https://olio.my/&" +
+    "response_type=code&" +
+    "scope=email%20profile";
+
+app.get("/login", (req, res) => {
+    res.redirect(googleUrl);
+});
+
 module.exports = app;
